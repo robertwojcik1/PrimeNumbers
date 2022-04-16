d1f3a0344e0e13aa75fd52ed636a01db6d72ca81
robert <robert.wojcik.programista@gmail.com>
2022-04-16 16:04:34 +0200

    initial commit

---
+++ src/main/java/PrimeNumbers.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static boolean isPrime (int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if( number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number, testNumber;
+        Scanner input = new Scanner(System.in);
+        testNumber = input.nextInt();
+
+        for (int j = 0; j < testNumber; j++) {
+            number = input.nextInt();
+            if (isPrime(number))
+                System.out.println("TAK");
+            else
+                System.out.println("NIE");
+        }
+    }
+}
